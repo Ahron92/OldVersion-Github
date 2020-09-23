a8dded207c410c55aefdd357aee9e575b5afc630
AhRonKwon <kwonahron@gmail.com>
2020-09-23 22:01:04 +0900

    Add media query by medium device

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//모듈화
+(function (window, document) {
+  "use strict"; //엄격한 자바스크립트를 적겠어라는 뜻
+
+  const $toggles = document.querySelectorAll(".toggle"); // NodeList 유사배열
+  const $toggleBtn = document.getElementById("toggle-btn");
+  //$를 적는 이유는 요소를 담고 있음을 알려주는 방법
+
+  $toggleBtn.addEventListener("click", function () {
+    toggleElements();
+  });
+
+  function toggleElements() {
+    [].forEach.call($toggles, function (toggle) {
+      toggle.classList.toggle("on");
+    });
+  }
+})(window, document);
